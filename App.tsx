7e9062fdd7619458516069ce4555b5335192db6c
Vikash Paudel <vikashpaudel@gmail.com>
2025-11-04 22:56:48 +0545

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Biography from "./pages/Biography";
+import Publications from "./pages/Publications";
+import Awards from "./pages/Awards";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Appointments from "./pages/Appointments";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/biography" element={<Biography />} />
+          <Route path="/publications" element={<Publications />} />
+          <Route path="/awards" element={<Awards />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/appointments" element={<Appointments />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
